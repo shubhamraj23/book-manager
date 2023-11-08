20eb465c9226d03c1ab977bfaaca7e9686a79699
Shubham Raj Pandit <shubhamraj465@gmail.com>
2023-11-08 12:10:32 +0530

    Created the express application

---
+++ index.js
@@ -0,0 +1,21 @@
+// Configure the application to use environment variables
+if (process.env.NODE_ENV !== 'prod'){
+  require('dotenv').config()
+}
+
+// Import all the relevant modules.
+const express = require('express')
+
+// Create an express application.
+const app = express()
+
+// Setup the middleware.
+app.use(express.json())
+
+// Setup the port for running the app.
+const PORT = process.env.PORT || 8000
+
+// Run the application.
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
