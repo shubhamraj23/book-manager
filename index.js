f8ad5a2fc7cf1108393ecadea47663e94d0e57dc
Shubham Raj Pandit <shubhamraj465@gmail.com>
2023-11-08 18:21:31 +0530

    Production Issue Fix

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
   console.log(process.env.NODE_ENV)
-  console.log(process.env.MONGO_URL)
+  console.log(process.env.MONGOURL)
 })
